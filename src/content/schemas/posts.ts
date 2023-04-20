b755cfff6e0c34ce417c01eb6fecef7ba4947b9d
Alex Harrison <alexjharrison@gmail.com>
2023-04-20 19:15:06 -0400

    remove unnecessary fields

--- src/content/schemas/posts.ts
+++ src/content/schemas/posts.ts
@@ -4,18 +4,10 @@ import type { CmsField } from "netlify-cms-core";
 export const postSchema = z.object({
   title: z.string(),
   date: z.date().transform(str => new Date(str)),
-  thumbnail: z.string(),
-  rating: z.number(),
 });
 
 export const postFields: CmsField[] = [
   { label: "Title", name: "title", widget: "string" },
-  { label: "Publish Date", name: "date", widget: "datetime" },
-  { label: "Featured Image", name: "thumbnail", widget: "image" },
-  {
-    label: "Rating (scale of 1-5)",
-    name: "rating",
-    widget: "number",
-  },
+  { label: "Meeting Date", name: "date", widget: "datetime" },
   { label: "Body", name: "body", widget: "markdown" },
 ];
